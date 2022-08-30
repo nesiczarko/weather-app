c835d1843850fc2885568dc83b1e5daf3b121b1d
Zarko Nesic <100737178+nesiczarko@users.noreply.github.com>
2022-08-30 02:53:03 +0200

    Weather app, first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '350b6a12e5mshb434d7986444005p138be5jsnd44f1bbd91d0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "60c0ba16e336c3181479d9359756bbf4";
